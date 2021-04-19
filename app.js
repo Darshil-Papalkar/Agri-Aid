9a8785e710120dfcd9f67e7c28a9d4677a6c4ff8
Darshil-Papalkar <papalkardarshil13@gmail.com>
2021-04-20 01:43:37 +0530

    app.js file modified

--- app.js
+++ app.js
@@ -107,6 +107,6 @@ app.post("/weather", function(req, res){
     }
 });
 
-app.listen(3000 || process.env.PORT, function(){
+app.listen(process.env.PORT || 3000, function(){
     console.log("Server started on port 3000");
 });
