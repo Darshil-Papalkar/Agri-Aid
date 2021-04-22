a20767a11290a1b58665ad9fd94dca9589b6020b
Darshil-Papalkar <papalkardarshil13@gmail.com>
2021-04-22 17:00:42 +0530

    form-backend

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const ejs = require("ejs");
 const lodash = require("lodash");
-const { round, toInteger } = require("lodash");
 const https = require("https");
-const { type } = require("os");
+const alert = require("alert");
+const mongoose = require("mongoose");
+const { raw } = require("express");
 
 const app = express();
 
@@ -11,9 +12,36 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+mongoose.set('useFindAndModify', false);
+mongoose.connect("mongodb://localhost:27017/farmAid", {useNewUrlParser: true, useUnifiedTopology: true });
+
+const userSchema = new mongoose.Schema({
+    f_name : String,
+    l_name : String,
+    email : String,
+    phone : String,
+    address_1 : String,
+    address_2 : String,
+    person_name : String,
+    city : String,
+    state : String,
+    zip : Number,
+    booking_date : Date
+});
+
+const User = mongoose.model("User", userSchema);
+
 function toCelcius(temp){
-    return String(round(temp - 273.16, 2))
+    return String(Math.round(temp - 273.16));
 }
+
+cities = {  "Vadodara" : "vd", 
+            "Chennai" : "ch", 
+            "Mumbai" : "mu", 
+            "Delhi" : "de", 
+            "Nagpur" : "na",
+            "Shimla" : "sh"
+        };
 
 app.get("/", function(req, res){
     res.render("index");
@@ -59,6 +87,22 @@ app.post("/form-send", function(req, res){
     const state = req.body.state;
     const zip = req.body.zip_code;
 
+    const newUser = new User({
+        f_name : f_name,
+        l_name : l_name,
+        email : email,
+        phone : phone,
+        address_1 : address_1,
+        address_2 : address_2,
+        person_name : person_name,
+        city : city,
+        state : state,
+        zip : zip,
+        booking_date : new Date()
+    });
+
+    newUser.save();
+    alert("Your Booking has been Successfully done");
     res.redirect("/dealer");
 });
 
@@ -98,44 +142,51 @@ app.post("/weather", function(req, res){
         res.redirect("weather");
     }
     else{
-        const apikey = "1698a076f54bd4a9cd70ad24c66c512c";
-        const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+apikey;
-        https.get(url, function(response){
-            response.on("data", (d) =>{
+        const query1 = lodash.capitalize(query);
+        if(!cities[query1]){
+            alert("Enter a Valid Indian City");
+            res.redirect("/weather");
+        }
+        else{
+            const apikey = "1698a076f54bd4a9cd70ad24c66c512c";
+            const url = "https://api.openweathermap.org/data/2.5/weather?q="+ query +"&appid="+apikey;
+            https.get(url, function(response){
+                response.on("data", (d) =>{
 
-                const weatherData =JSON.parse(d);
-                const temp = weatherData.main.temp;
-                const icon = weatherData.weather[0].icon;
-                const imgURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
-                const lat = weatherData.coord.lat;
-                const lon = weatherData.coord.lon;
-                const temp_cel = toCelcius(temp);
-                const pressure = weatherData.main.pressure;
-                const humidity = weatherData.main.humidity;
-                const cityname = weatherData.name;
-                const cityid = weatherData.id;
-                const countryName = weatherData.sys.country;
-                const feels = weatherData.main.feels_like;
-                const max_temp = weatherData.main.temp_max;
-                const min_temp = weatherData.main.temp_min;
-                
-                res.render("weather_op", {
-                    coordinate_lat : lat,
-                    coordinate_lon : lon,
-                    temp : temp,
-                    img: imgURL,
-                    temp_cel : temp_cel,
-                    pressure : pressure,
-                    humidity : humidity,
-                    cityname : cityname,
-                    cityid : cityid,
-                    countryName : countryName,
-                    feel_like: feels,
-                    max_temp : max_temp,
-                    min_temp : min_temp
+                    const weatherData =JSON.parse(d);
+                    const temp = weatherData.main.temp;
+                    const icon = weatherData.weather[0].icon;
+                    const imgURL = "http://openweathermap.org/img/wn/"+icon+"@2x.png";
+                    const lat = weatherData.coord.lat;
+                    const lon = weatherData.coord.lon;
+                    const temp_cel = toCelcius(temp);
+                    const pressure = weatherData.main.pressure;
+                    const humidity = weatherData.main.humidity;
+                    const cityname = weatherData.name;
+                    const cityid = weatherData.id;
+                    const countryName = weatherData.sys.country;
+                    const feels = weatherData.main.feels_like;
+                    const max_temp = weatherData.main.temp_max;
+                    const min_temp = weatherData.main.temp_min;
+                    
+                    res.render("weather_op", {
+                        coordinate_lat : lat,
+                        coordinate_lon : lon,
+                        temp : temp,
+                        img: imgURL,
+                        temp_cel : temp_cel,
+                        pressure : pressure,
+                        humidity : humidity,
+                        cityname : cityname,
+                        cityid : cityid,
+                        countryName : countryName,
+                        feel_like: feels,
+                        max_temp : max_temp,
+                        min_temp : min_temp
+                    });
                 });
             });
-        });
+        }
     }
 });
 
